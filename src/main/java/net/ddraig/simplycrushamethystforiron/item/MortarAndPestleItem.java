1fbc499e94a5c5ebbfce493c527386bd5ca99806
AzureDDraig <107419858+AzureDDraig@users.noreply.github.com>
2023-11-25 02:24:14 -0600

    1.0.0 - Initial Commit

---
+++ src/main/java/net/ddraig/simplycrushamethystforiron/item/MortarAndPestleItem.java
@@ -0,0 +1,23 @@
+
+package net.ddraig.simplycrushamethystforiron.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+
+public class MortarAndPestleItem extends Item {
+	public MortarAndPestleItem() {
+		super(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem() {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		return new ItemStack(this);
+	}
+}
